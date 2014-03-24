a370ff1f787373968056eca578486b8c13a62eb2
= <kayakatu@yahoo.co.jp>
2014-03-24 23:02:02 +0900

    Initial Commit

---
+++ Chiebukuro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chiebukuro
+//
+//  Created by 萱島克英 on 2014/03/24.
+//  Copyright (c) 2014年 private_company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChiebukuroTests/ChiebukuroTests.m
@@ -0,0 +1,34 @@
+//
+//  ChiebukuroTests.m
+//  ChiebukuroTests
+//
+//  Created by 萱島克英 on 2014/03/24.
+//  Copyright (c) 2014年 private_company. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChiebukuroTests : XCTestCase
+
+@end
+
+@implementation ChiebukuroTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
